a0696331662d5b30a96e80194af4c11ba8379fac
3pedru <3pedru@gmail.com>
2013-05-17 12:05:51 -0300

    initial import

---
+++ testesashagrey/src/testesashagrey/TestesashagreyServlet.java
@@ -0,0 +1,13 @@
+package testesashagrey;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class TestesashagreyServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
